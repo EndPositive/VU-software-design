bde3a5f7aee290d828fa6efff98ed11beda9bbec
Jop Zitman <j.zitman@vu.nl>
2021-02-26 15:55:07 +0100

    Merge project with LibGDX template (https://libgdx.com/dev/project-generation/)

---
+++ src/main/java/BreachGame.java
@@ -0,0 +1,28 @@
+package main.java;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class BreachGame extends ApplicationAdapter {
+	SpriteBatch batch;
+	
+	@Override
+	public void create () {
+		batch = new SpriteBatch();
+	}
+
+	@Override
+	public void render () {
+		Gdx.gl.glClearColor(0, 1, 0, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		batch.begin();
+		batch.end();
+	}
+	
+	@Override
+	public void dispose () {
+		batch.dispose();
+	}
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,11 @@
+package main.java;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
 public class Main {
-    public static void main (String[] args){
-        System.out.println("Welcome to Software Design");
-    }
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new BreachGame(), config);
+	}
 }
