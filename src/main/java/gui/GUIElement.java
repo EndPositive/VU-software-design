a353c8580c159525e46432f9ac52b64b1c178c89
ChrisPC-39 <60066382+ChrisPC-39@users.noreply.github.com>
2021-02-28 14:56:54 +0100

    Fixed small variable

--- src/main/java/gui/GUIElement.java
+++ src/main/java/gui/GUIElement.java
@@ -3,11 +3,13 @@ package main.java.gui;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import main.java.gamelogic.GameFrame;
+import main.java.gamelogic.GameLevel;
 
 public abstract class GUIElement {
     private SpriteBatch batch;
     private BitmapFont font;
     private float x, y;
+    private GameLevel gameLevel;
 
     //TODO: Implement me
     public GUIElement(float x, float y) {
