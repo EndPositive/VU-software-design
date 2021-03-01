b24cffb0e3f22c5d814e01bdccac38e4267cc8d3
cosieqy <32099671+cosieqy@users.noreply.github.com>
2021-03-01 21:40:36 +0100

    implement scoreGUI

--- src/main/java/gui/ScoreGUI.java
+++ src/main/java/gui/ScoreGUI.java
@@ -1,5 +1,6 @@
 package main.java.gui;
 
+import com.badlogic.gdx.graphics.Color;
 import main.java.gamelogic.GameState;
 
 public class ScoreGUI extends GUIElement {
@@ -10,5 +11,15 @@ public class ScoreGUI extends GUIElement {
     // TODO: Yingdi Assignment 3 simple GUI that displays the score from gameState.getScore() at x,y
     @Override
     public void render(GameState gameState) {
+        batch.begin();
+
+        font.setColor(Color.PINK);
+        font.draw(batch, "You breached the protocol!", x, y);
+        font.draw(batch, "Your score is: ", x, y - 30);
+
+        font.setColor(Color.GOLD);
+        font.draw(batch, String.valueOf(gameState.getScore()), x + 10, y - 60);
+
+        batch.end();
     }
 }
