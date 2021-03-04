147fabaf1add084126d39eb38e7f6f7b26012662
ChrisPC-39 <60066382+ChrisPC-39@users.noreply.github.com>
2021-03-04 16:03:37 +0100

    Improved Easter Egg Screen ;)

--- src/main/java/screens/EasterEggScreen.java
+++ src/main/java/screens/EasterEggScreen.java
@@ -4,9 +4,11 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.InputAdapter;
 import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 
 import static com.badlogic.gdx.graphics.GL20.GL_COLOR_BUFFER_BIT;
 
@@ -38,11 +40,23 @@ public class EasterEggScreen extends ScreenAdapter {
     public void render(float delta) {
         Gdx.gl.glClear(GL_COLOR_BUFFER_BIT);
 
+        ShapeRenderer shapeRenderer = new ShapeRenderer();
+        shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
+        shapeRenderer.setColor(Color.ORANGE);
+        shapeRenderer.rect(65, 233, 30, 20);
+        shapeRenderer.end();
+
         batch.begin();
         batch.draw(tibiImg, 0, 0, 200, 200);
         batch.draw(chrisImg, 200, 0, 200, 200);
         batch.draw(jopImg, 400, 0, 200, 200);
-        font.draw(batch, "This is an easter egg", 20, 250);
+
+        font.setColor(Color.WHITE);
+        font.draw(batch, "Easter", 20, 250);
+
+        font.setColor(Color.BLACK);
+        font.draw(batch, "egg", 67, 250);
+
         batch.end();
     }
 }
