4484e5f31ffe9492a3d2a56ff75fa07926d93fd2
ChrisPC-39 <sebi.iozu@gmail.com>
2021-03-17 12:08:05 +0100

    Added comments

--- src/main/java/gui/GridGUI.java
+++ src/main/java/gui/GridGUI.java
@@ -11,6 +11,8 @@ public class GridGUI extends GUIElement {
 
     @Override
     public void render(GameState gameState) {
+
+        //Render the gray area spanning the entire row/column that the user can navigate
         ShapeRenderer shapeRenderer = new ShapeRenderer();
         shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
         shapeRenderer.setColor(Color.GRAY);
@@ -23,6 +25,7 @@ public class GridGUI extends GUIElement {
         }
         shapeRenderer.end();
 
+        //Render the cell selector
         shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
         shapeRenderer.setColor(Color.BLUE);
         shapeRenderer.rect(x + gameState.selector.x * 30, y - gameState.selector.y * 30 - 15, 20, 20);
@@ -30,6 +33,7 @@ public class GridGUI extends GUIElement {
 
         batch.begin();
 
+        //Render the matrix
         gameState.gameLevel.matrix.forEach((key, value) -> font.draw(batch, value, x + key.x * 30, y - key.y * 30));
 
         batch.end();
