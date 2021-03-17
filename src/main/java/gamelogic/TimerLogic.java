20642ecd7c596d8189ba3f786f09cc3827c97447
ChrisPC-39 <sebi.iozu@gmail.com>
2021-03-17 12:00:07 +0100

    Added comments explaining 'time'

--- src/main/java/gamelogic/TimerLogic.java
+++ src/main/java/gamelogic/TimerLogic.java
@@ -4,8 +4,11 @@ public class TimerLogic {
     public long startTime = -1;
     public final long totalTime;
 
+    //'time' is multiplied by 1000 to convert it from milliseconds to seconds.
+    //'time' is incremented by 1 to make the countdown starts at the given 'time'.
+    //The countdown is decreased when ENTER is pressed, making the countdown appear as if it starts at 'timer - 1' if it's not incremented by 1.
     public TimerLogic(long time) {
-        totalTime = time * 1000 + 1;
+        totalTime = time * 1000;
     }
 
     public boolean hasStarted() {
