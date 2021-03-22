a843e9fe9d22745adde47283a71f61b2f32bb356
Jop Zitman <j.zitman@student.vu.nl>
2021-03-22 23:44:08 +0100

    Move Buffer initialization from constructor to init

--- src/main/java/gamelogic/GameState.java
+++ src/main/java/gamelogic/GameState.java
@@ -12,13 +12,12 @@ public class GameState {
     public final TimerLogic timerLogic;
     public final Deque<UndoRedoCommand> commandStack = new ArrayDeque<>();
     public final Deque<UndoRedoCommand> redoCommandStack = new ArrayDeque<>();
-    public final Buffer buffer;
+    public final Buffer buffer = new Buffer(this);
 
     public GameState(GameLevel gameLevel) {
         this.gameLevel = gameLevel;
         selector = new Cell(0, 0);
         timerLogic = new TimerLogic(10);
-        buffer = new Buffer(this);
     }
 
     public boolean isGameOver() {
