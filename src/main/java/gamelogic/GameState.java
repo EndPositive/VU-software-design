fe423c780b932dbbc037358a0238b5978b83eafd
Jop Zitman <j.zitman@student.vu.nl>
2021-03-22 23:44:54 +0100

    Move selector initialization from constructor to init

--- src/main/java/gamelogic/GameState.java
+++ src/main/java/gamelogic/GameState.java
@@ -10,7 +10,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class GameState {
-    public Cell selector;
+    public Cell selector = new Cell(0, 0);
     public final GameLevel gameLevel;
     public final TimerLogic timerLogic;
     public final Deque<UndoRedoCommand> commandStack = new ArrayDeque<>();
@@ -20,7 +20,6 @@ public class GameState {
 
     public GameState(GameLevel gameLevel) {
         this.gameLevel = gameLevel;
-        selector = new Cell(0, 0);
         timerLogic = new TimerLogic(10);
     }
 
