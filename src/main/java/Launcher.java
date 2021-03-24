a54c5ee89e8de99cb346c8816dd703679145f0b5
Jop Zitman <j.zitman@student.vu.nl>
2021-03-24 19:50:10 +0100

    Rename Main to Launcher

--- src/main/java/Launcher.java
+++ src/main/java/Launcher.java
@@ -3,7 +3,7 @@ package main.java;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 
-public class Main {
+public class Launcher {
     public static void main(String[] args) {
         if (args.length < 1) {
             System.err.println("No input file given");
