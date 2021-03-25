b7335fd0601af89831903f90109f326db29e2c0e
cosieqy <32099671+cosieqy@users.noreply.github.com>
2021-03-25 11:14:52 +0100

    change deque back to stack

--- src/main/java/gamelogic/GameState.java
+++ src/main/java/gamelogic/GameState.java
@@ -4,15 +4,14 @@ import main.java.commands.UndoRedoCommand;
 import main.java.misc.Cell;
 import main.java.misc.Timer;
 
-import java.util.ArrayDeque;
-import java.util.Deque;
+import java.util.Stack;
 
 public class GameState {
     public Cell selector = new Cell(0, 0);
     public final GameLevel gameLevel;
     public final Timer timer = new Timer(10);
-    public final Deque<UndoRedoCommand> commandHistory = new ArrayDeque<>();
-    public final Deque<UndoRedoCommand> commandFuture = new ArrayDeque<>();
+    public final Stack<UndoRedoCommand> commandHistory = new Stack<>();
+    public final Stack<UndoRedoCommand> commandFuture = new Stack<>();
     public final Buffer buffer = new Buffer(this);
 
     public GameState(GameLevel gameLevel) {
